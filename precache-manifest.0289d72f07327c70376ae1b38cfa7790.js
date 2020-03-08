7b8c5a34d6332098bbdaed359da527ff832e1806
nastyarisa <pl_nastya@mail.ru>
2020-03-08 22:42:50 +0300

    Updates

---
+++ precache-manifest.0289d72f07327c70376ae1b38cfa7790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b665dec3df41c07ea1303758880008d0",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "ab8fc31525e8cd494453",
+    "url": "/todo_app/static/css/main.69f75f1c.chunk.css"
+  },
+  {
+    "revision": "6450a5252d72153b8044",
+    "url": "/todo_app/static/js/2.06a09821.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo_app/static/js/2.06a09821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8fc31525e8cd494453",
+    "url": "/todo_app/static/js/main.d1a1db80.chunk.js"
+  },
+  {
+    "revision": "2e0faa8b0f532b8f5ab6",
+    "url": "/todo_app/static/js/runtime-main.0e552caa.js"
+  }
+]);
